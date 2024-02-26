deea6710bf6d22642761fb08c3e935ee3da462df
thenatural86 <philip.smalls86@gmail.com>
2024-02-26 13:48:17 -0300

    request object

--- app.js
+++ app.js
@@ -1,11 +1,21 @@
-const { response } = require('express')
 const http = require('http')
 
 const server = http.createServer((req, res) => {
-  console.log('user hit the server')
-  res.writeHead(200, { 'content-type': 'text/html' })
-  res.write('<h1>home page</h1>')
-  res.end()
+  console.log(req)
+  const url = req.url
+  if (url === '/') {
+    res.writeHead(200, { 'content-type': 'text/html' })
+    res.write('<h1>home page</h1>')
+    res.end()
+  } else if (url === '/about') {
+    res.writeHead(200, { 'content-type': 'text/html' })
+    res.write('<h1>About page</h1>')
+    res.end()
+  } else {
+    res.writeHead(404, { 'content-type': 'text/html' })
+    res.write('<h1>Error: Page Not Found</h1>')
+    res.end()
+  }
 })
 
 server.listen(5000)
