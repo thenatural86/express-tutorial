65caf5b37be0027302e249eada6a2402ea8f96be
thenatural86 <philip.smalls86@gmail.com>
2024-02-27 17:24:49 -0300

    query string params

--- app.js
+++ app.js
@@ -27,6 +27,26 @@ app.get('/api/products/:productID', (req, res) => {
   return res.json(singleProduct)
 })
 
+app.get('/api/v1/query', (req, res) => {
+  // console.log(req.query)
+  const { search, limit } = req.query
+  let sortedProducts = [...products]
+
+  if (search) {
+    sortedProducts = sortedProducts.filter((product) => {
+      return product.name.startsWith(search)
+    })
+  }
+  if (limit) {
+    sortedProducts = sortedProducts.slice(0, Number(limit))
+  }
+  if (sortedProducts.length < 1) {
+    // return res.status(200).send('No products match your search')
+    return res.status(200).json({ success: true, data: [] })
+  }
+  return res.status(200).json(sortedProducts)
+})
+
 app.listen(5000, () => {
   console.log('Server is listening on Port : 5000....')
 })
