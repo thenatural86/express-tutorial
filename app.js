68ba086dd3928d71d7a5096a10d6ce19274ba6f5
thenatural86 <philip.smalls86@gmail.com>
2024-03-01 15:39:56 -0300

    get method

--- app.js
+++ app.js
@@ -1,6 +1,18 @@
 const express = require('express')
 const app = express()
+const { people } = require('./data')
+
+app.get('/', (req, res) => {
+  console.log('Hello World!')
+  res.send('Home')
+})
+
+app.get('/api/people', (req, res) => {
+  res.status(200).json({ success: true, data: people })
+  // res.send()
+})
 
 app.listen(5000, () => {
+  console.log(people)
   console.log('Server is listening on Port : 5000....')
 })
