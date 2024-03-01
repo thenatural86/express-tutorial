f51a44f7777a5ac45dd904e4934b11a7eae45192
thenatural86 <philip.smalls86@gmail.com>
2024-03-01 15:23:03 -0300

    middleware options

---
+++ middleware-options.js
@@ -0,0 +1,24 @@
+const morgan = require('morgan')
+const logger = require('./logger')
+const authorize = require('./authorize')
+// req => middleware => res
+
+// app.use([logger, authorize])
+// app.use(express.static('./public'))
+app.use(morgan('tiny'))
+app.get('/', (req, res) => {
+  res.send('Home')
+})
+
+app.get('/about', (req, res) => {
+  res.send('About')
+})
+
+app.get('/api/products', (req, res) => {
+  res.send('About')
+})
+
+app.get('/api/items', (req, res) => {
+  console.log(req.user)
+  res.send('About')
+})
