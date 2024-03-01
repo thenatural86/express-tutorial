db5e2563db392c02315de5ef2a3dbb3ca49bfcbf
thenatural86 <philip.smalls86@gmail.com>
2024-03-01 16:33:49 -0300

    post form example

--- app.js
+++ app.js
@@ -2,16 +2,22 @@ const express = require('express')
 const app = express()
 const { people } = require('./data')
 
+// static assets
 app.use(express.static('./methods-public'))
-
-// app.get('/', (req, res) => {
-//   console.log('Hello World!')
-//   res.send('Home')
-// })
+// parse form data
+app.use(express.urlencoded({ extended: false }))
 
 app.get('/api/people', (req, res) => {
   res.status(200).json({ success: true, data: people })
-  // res.send()
+})
+
+app.post('/login', (req, res) => {
+  const { name } = req.body
+  if (name) {
+    return res.status(200).send(`Welcome ${name}`)
+  }
+
+  res.status(401).send('Please Provide Credentials')
 })
 
 app.listen(5000, () => {
